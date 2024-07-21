b37a0e9fd86b7d44910d6d5b47a5d2fbbcbd74d2
Akash Sonowal <work.akashsonowal@gmail.com>
2024-07-21 23:24:30 +0530

    update

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -8,6 +8,6 @@ headers = {
 class LLMServiceUser(HttpUser):
     @task
     def endpoint(self):
-        # data = {"prompt": "Generate a short story about a mysterious adventure."}
-        response = self.client.post("/", headers=headers)#, json=data)
+        data = {"prompt": "Generate a short story about a mysterious adventure.", "new_tokens": 100}
+        response = self.client.post("/", headers=headers, json=data)
         response.raise_for_status()
