ca330202ab2f05009ac71739d75f18e96e6e0c45
Ubuntu <ubuntu@ip-172-31-4-83.ap-south-1.compute.internal>
2024-07-21 09:03:45 +0000

    add update

--- src/llama_serve/app.py
+++ src/llama_serve/app.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from uvicorn import Config, Server
 
-# from .batching import BatchQueue
+from batching import BatchQueue
 
 logger = logging.getLogger(__name__)
 
@@ -21,29 +21,29 @@ class MLServe:
         input_schema: Optional[BaseModel],
         response_schema: Optional[BaseModel],
         max_batch_size: int = 16,
-#         batch_wait_timeout_s: int = 0.05,
+        batch_wait_timeout_s: int = 0.05,
     ):
         self.loop = asyncio.get_event_loop()
-#         self.queue = BatchQueue(self.loop, max_batch_size, batch_wait_timeout_s, handle)
-#         self.app = FastAPI(title="MLServe: A scalable ML Server", docs_url="/")
-#         self.config = Config(app=self.app, loop=self.loop, host="0.0.0.0", port=8080)
-#         self.server = Server(self.config)
+        self.queue = BatchQueue(self.loop, max_batch_size, batch_wait_timeout_s, handle)
+        self.app = FastAPI(title="MLServe: A scalable ML Server", docs_url="/")
+        self.config = Config(app=self.app, loop=self.loop, host="0.0.0.0", port=8080)
+        self.server = Server(self.config)
 
-#         INPUT_SCHEMA = input_schema
-#         RESPONSE_SCHEMA = response_schema
+        INPUT_SCHEMA = input_schema
+        RESPONSE_SCHEMA = response_schema
 
         def health():
             return Response(status_code=200)
 
-#         async def api(request: INPUT_SCHEMA) -> RESPONSE_SCHEMA:
-#             try:
-#                 return await self.queue.process_request(request)
-#             except Exception as e:
-#                 logger.exception("Error processing request")
-#                 raise HTTPException(status_code=500, detail="Internal Server Error")
+        async def api(request: INPUT_SCHEMA) -> RESPONSE_SCHEMA:
+            try:
+                return await self.queue.process_request(request)
+            except Exception as e:
+                logger.exception("Error processing request")
+                raise HTTPException(status_code=500, detail="Internal Server Error")
 
         self.app.add_api_route("/health", health, methods=["GET"])
-#         self.app.add_api_route("/endpoint", api, methods=["POST"])
+        self.app.add_api_route("/endpoint", api, methods=["POST"])
 
         self.app.add_middleware(
             CORSMiddleware,
--- src/llama_serve/inference.py
+++ src/llama_serve/inference.py
@@ -1,64 +1,269 @@
-import time
 import copy
-import torch
+import time 
+import torch 
+import random
+from tqdm import tqdm 
+import torch.nn.functional as F
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-class LLM:
-    def __init__(self, model_name, quantize=False) -> None:
-        self._setup_model(model_name, quantize)
-        self._bs = 8
+model_name = "/home/ubuntu/llama-serve/artifacts/gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Define PAD Token = EOS Token = 50256
+tokenizer.pad_token = tokenizer.eos_token
+model.config.pad_token_id = model.config.eos_token_id
+
+# pad on the left so we can append new tokens on the right
+tokenizer.padding_side = "left"
+tokenizer.truncation_side = "left"
+
+def init_batch(requests):
+    prompts = [r[0] for r in requests] # the 0 is for prompt in the input tuple
+    inputs = tokenizer(prompts, padding=True, return_tensors="pt")
+    attention_mask = inputs["attention_mask"]
+    position_ids = attention_mask.long().cumsum(-1) - 1
+    position_ids.masked_fill_(attention_mask == 0, 1) # all pad tokes are filled with 1
+    return {
+        "position_ids": position_ids,
+        "responses": copy.copy(prompts), # dummy placeholder
+        "tokens_remaining": [r[1] for r in requests], # num tokens
+        **inputs
+    }
+
+def generate_next_token(batch):
+    inputs = copy.copy(batch)
+    inputs.pop("responses")
+    inputs.pop("tokens_remaining")
+    next_token_ids, past_key_values = generate_batch_tokens_with_past(inputs)
+    next_tokens = tokenizer.batch_decode(next_token_ids)
+    return get_next_inputs(batch, next_token_ids, past_key_values, next_tokens)
+
+def generate_batch_tokens_with_past(inputs):
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    logits = outputs.logits
+    last_logits = logits[:, -1, :]
+    next_token_ids = last_logits.argmax(dim=1)
+    return next_token_ids, outputs.past_key_values
+
+def get_next_inputs(batch, next_token_ids, past_key_values, next_tokens):
+    return {
+        # '-1' here means the remaining elements for this dim
+        "input_ids": next_token_ids.reshape((-1, 1)),
+        # increment last, discard the rest
+        "position_ids": batch["position_ids"][:, -1].unsqueeze(-1) + 1,
+        # concatenate vector of 1's with shape [batch_size]
+        "attention_mask": torch.cat([
+            batch["attention_mask"],
+            torch.ones((next_token_ids.shape[0], 1)),  
+        ], dim=1),
+        "past_key_values": past_key_values,
+        "responses": [
+            r1 + r2 for r1, r2 
+            in zip(batch["responses"], next_tokens)],
+        "tokens_remaining": [
+            v - 1 for v in batch["tokens_remaining"]],
+    }
+
+def filter_batch(batch):
+    # mark all rows with 0 tokens remaining for removal
+    remove_indices = []
+    for i, tokens_remaining in enumerate(batch["tokens_remaining"]):
+        if tokens_remaining <= 0:
+            remove_indices.append(i)
     
-    def _setup_model(self, model_name, quantize):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+    # first, define a mask used to subselect the indices to keep from each tensor, given the indices to remove
+    batch_size = batch["input_ids"].size(0)
+    mask = torch.ones(batch_size, dtype=torch.bool)
+    mask[remove_indices] = False
+
+    # index into the tensors using the mask to remove rows
+    input_ids = batch["input_ids"][mask]
+    position_ids = batch["position_ids"][mask]
+    attention_mask = batch["attention_mask"][mask]
+
+    responses = [
+        r 
+        for i, r in enumerate(batch["responses"])
+        if i not in remove_indices
+    ]
+
+    finished_reponses = [r for i, r in enumerate(batch["responses"]) if i in remove_indices]
+
+    tokens_remaining = [
+        v 
+        for i, v in enumerate(batch["tokens_remaining"])
+        if i not in remove_indices
+    ]
+
+    past_key_values = batch["past_key_values"]
+    new_past_key_values = []
+
+    for i in range(len(past_key_values)):
+        k, v = past_key_values[i]
+
+        k = k[mask]
+        v = v[mask]
+        new_past_key_values.append((k, v))
+
+    past_key_values = new_past_key_values
+    if input_ids.size(0) > 0:
+        # next, as an optimization to avoid wasting 
+        # compute cycles on padding tokens,
+        # we will left truncate the attention_mask 
+        # and past_key_values to the longest
+        # remaining sequence length
+        # we obtain the longest sequence length by 
+        # looking for the min first non-zero index
+        # of the attention mask
+        # cumprod ensures we stop accumulating when we see a 1
+        zero_mask = attention_mask == 0
+        cumprod = zero_mask.cumprod(dim=1)  
+        leading_zeros_count = cumprod.sum(dim=1)
+        min_leading_zeros = torch.min(leading_zeros_count)
+        truncation_offset = min_leading_zeros.item()
+
+        # do the trunction
+        attention_mask = attention_mask[:, truncation_offset:]
+        past_key_values = past_key_values
+        new_past_key_values = []
+        for i in range(len(past_key_values)):
+            k, v = past_key_values[i]
+            k = k[:, :, truncation_offset:, :]
+            v = v[:, :, truncation_offset:, :]
+            new_past_key_values.append((k, v))
+        past_key_values = new_past_key_values
     
-    def _init_batch(self):
-        "prefill"
-        pass 
+    # return the new batch
+    return {
+        "input_ids": input_ids,
+        "position_ids": position_ids,
+        "attention_mask": attention_mask,
+        "past_key_values": past_key_values,
+        "responses": responses,
+        "tokens_remaining": tokens_remaining,
+    }, remove_indices, finished_reponses
 
-    def merge_batches(self):
-        pass 
-
-    def filter_batch(self):
-        pass 
-
-    def generate_next_token(self, batch):
-        pass 
-
-    def generate_reponse(self, prompts):
-        pass 
-
+def merge_batches(batch1, batch2):
+    # first find the max sequence length of the two batches
+    # this can be obtained from the second dimension 
+    # of the attention mask
+    attn_mask1 = batch1["attention_mask"]
+    attn_mask2 = batch2["attention_mask"]
+    max_seq_len = max(attn_mask1.shape[1], attn_mask2.shape[1])
     
-    @torch.no_grad()
-    def generate(self, requests):
-        request_queue = copy.copy(requests)
-        responses = [None] * len(requests)
+    # pad each mask (on the left) to the max sequence length
+    # attention mask uses 0 for padding
+    padding1 = max_seq_len - attn_mask1.shape[1]
+    padding2 = max_seq_len - attn_mask2.shape[1]
+    attn_mask1 = F.pad(attn_mask1, (padding1, 0), "constant", 0)
+    attn_mask2 = F.pad(attn_mask2, (padding2, 0), "constant", 0)
+    
+    # because we only append batches post decoding, 
+    # we don't need to pad input_ids
+    # or position_ids. these are always length 1 
+    # in the sequence dimension
+    # however, we do need to pad the 
+    # past_key_values, which have shape:
+    # [batch_size, num_heads, sequence_length, head_dim]
+    past_kv1 = batch1["past_key_values"]
+    past_kv2 = batch2["past_key_values"]
+    
+    padded_kv1 = []
+    for i in range(len(past_kv1)):
+        k, v = past_kv1[i]
+        k = F.pad(k, (0, 0, padding1, 0), "constant", 0)
+        v = F.pad(v, (0, 0, padding1, 0), "constant", 0)     
+        padded_kv1.append((k, v))
+    
+    padded_kv2 = []
+    for i in range(len(past_kv2)):
+        k, v = past_kv2[i]
+        k = F.pad(k, (0, 0, padding2, 0), "constant", 0)
+        v = F.pad(v, (0, 0, padding2, 0), "constant", 0)     
+        padded_kv2.append((k, v))
+        
+    # now that everything has been padded to have
+    # consistent shapes, let's merge
+    input_ids = torch.concat(
+        [batch1["input_ids"], batch2["input_ids"]], dim=0)
+    position_ids = torch.concat(
+        [batch1["position_ids"], batch2["position_ids"]], dim=0) 
+    attn_mask = torch.concat([attn_mask1, attn_mask2], dim=0)
+    
+    past_kv = []
+    for i in range(len(padded_kv1)):
+        k1, v1 = padded_kv1[i]
+        k2, v2 = padded_kv2[i]
+        k = torch.concat([k1, k2], dim=0)
+        v = torch.concat([v1, v2], dim=0)
+        past_kv.append((k, v))
+    
+    return {
+        "input_ids": input_ids,
+        "position_ids": position_ids,
+        "attention_mask": attn_mask,
+        "past_key_values": past_kv,
+        "responses": batch1["responses"] + batch2["responses"],
+        "tokens_remaining": batch1["tokens_remaining"] + batch2["tokens_remaining"],
+    }
 
-        batch = self._init_batch(request_queue[:self._bs])
+queue_size = 32
+batch_size = 8
 
-        cached_batch = self.generate_next_token(batch)
-        request_queue = request_queue[self._bs:]
+prompts = [
+    "The quick brown fox jumped over the",
+    "The rain in Spain falls",
+    "What comes up must",
+]
 
-        # continue until both the request queue is fully drained and every input
-        # within the cached_batch has completed generation
+random.seed(42)
 
-        while len(request_queue) > 0 or cached_batch["input_ids"].size(0) > 0:
-            batch_capacity = self._bs - cached_batch["input_ids"].size(0)
+# requests waiting to be processed
+# requests are tuples (prompt, max_tokens)
+request_queue = [
+    (random.choice(prompts), 100 if i % batch_size == 0 else 10)
+    for i in range(queue_size)
+]
 
-            if batch_capacity > 0 and len(request_queue) > 0:
-                # prefill
-                new_batch = self.init_batch(request_queue[:batch_capacity])
-                new_batch = self.generate_next_token(new_batch)
-                request_queue = request_queue[batch_capacity:]
+t0 = time.time()
+with tqdm(total=len(request_queue), desc=f"bs={batch_size}") as pbar:
+    # first, let's seed the initial cached_batch with the first `batch_size` inputs and run the initial prefill step
+    batch = init_batch(request_queue[:batch_size])
+    cached_batch = generate_next_token(batch) # prefill step
 
-                cached_batch = self.merge_batches(cached_batch, new_batch)
-            
-            # decode
-            cached_batch = self.generate_next_token(cached_batch)
+    request_queue = request_queue[batch_size:] # we clean the request queue as we read the first batch
 
-            # remove any inputs that have finished generation
-            cached_batch, removed_indices, completed_responses = self.filter_batch(cached_batch)
+    # continue until both the request queue is 
+    # fully drained and every input
+    # within the cached_batch has completed generation
 
-            for idx, resp in zip(removed_indices, completed_responses):
-                responses[idx] = resp
-        return responses
+    while (len(request_queue) > 0 or cached_batch["input_ids"].size(0) > 0):
+        batch_capacity = batch_size - cached_batch["input_ids"].size(0)
+
+        if batch_capacity > 0 and len(request_queue) > 0:
+            # pull more items from queue to fill capacity
+            new_batch = init_batch(request_queue[:batch_capacity])
+            new_batch = generate_next_token(new_batch)
+
+            request_queue = request_queue[batch_capacity:] # self.consume(batch_capacity)
+
+            # merge new batch with current batch
+            cached_batch = merge_batches(cached_batch, new_batch)
+
+        # decode
+        cached_batch = generate_next_token(cached_batch)
+
+        # remove any inputs that have finished generation
+        cached_batch, removed_indices, finished_responses = filter_batch(cached_batch)
+
+        if removed_indices:
+            print(len(finished_responses))
+
+        pbar.update(len(removed_indices))
+
+duration_s = time.time() - t0
+print("duration", duration_s)
